b798146b38619e2cde2ed193c2f245f99b9f56ca
Johan Edlund <me@johanedlund.se>
2022-03-28 21:16:45 +0000

    Add my own site

---
+++ _data/sites/johanedlund.js
@@ -0,0 +1,17 @@
+module.exports = {
+	name: "johanedlund.se", // optional, falls back to object key
+	description: "Personal website of Johan Edlund",
+	// skip if localhost
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		//freshChrome: "site"
+	},
+	urls: [
+		"https://www.johanedlund.se/",
+		"https://www.johanedlund.se/cv/"
+	]
+};
