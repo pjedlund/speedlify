2cd8c654ddecf4f912eec656ccdd3805b5efa3d9
Johan Edlund <me@johanedlund.se>
2024-08-10 12:49:39 +0200

    removed johanedlund.se/cv

--- _data/sites/johanedlund.js
+++ _data/sites/johanedlund.js
@@ -1,13 +1,9 @@
 module.exports = {
-	name: "johanedlund.se",
-	description: "johanedlund.se",
-	options: {
-		frequency: 60 * 2, // 2 hour
-		freshChrome: "site"
-	},
-	urls: [
-		"https://www.johanedlund.se/",
-		"https://www.johanedlund.se/posts/this-is-the-first-example-post/",
-		"https://www.johanedlund.se/cv/"
-	]
+  name: 'johanedlund.se',
+  description: 'johanedlund.se',
+  options: {
+    frequency: 60 * 2, // 2 hour
+    freshChrome: 'site',
+  },
+  urls: ['https://www.johanedlund.se/', 'https://www.johanedlund.se/posts/this-is-the-first-example-post/'],
 };
