c18f25854eccb10f694eec9b60ffef6cd778faf1
Johan Edlund <johanedlund@johans-mac-mini.local>
2024-11-24 15:36:14 +0100

    removed telavox.com

--- _data/sites/telavox.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-          name: 'telavox.com',
-          description: 'telavox.com',
-          options: {
-                    frequency: 60 * 12, // 12 hour
-                    freshChrome: 'site',
-          },
-          urls: ['https://www.telavox.com/'],
-};
