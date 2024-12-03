25a2349f10202c62fa4e201fd0e67364bfbfe22d
Johan Edlund <me@johanedlund.se>
2024-12-03 20:56:03 +0100

    dgdgdgf

--- _data/sites/johanedlund.js
+++ _data/sites/johanedlund.js
@@ -5,8 +5,5 @@ module.exports = {
     frequency: 60 * 23, // 24 hour
     freshChrome: 'site',
   },
-  urls: [
-    'https://www.johanedlund.se/',
-    'https://www.johanedlund.se/posts/this-is-the-first-example-post/',
-  ],
+  urls: ['https://www.johanedlund.se/'],
 };
