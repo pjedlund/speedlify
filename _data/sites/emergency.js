2c0b133b140ed3af2a35be4926896990fd511b82
Johan Edlund <johanedlund@johans-mac-mini.local>
2024-12-03 12:44:30 +0100

    updated sources in _data

---
+++ _data/sites/emergency.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'emergency.johanedlund.se',
+  description: 'johanedlund.se',
+  options: {
+    frequency: 60 * 2, // 2 hour
+    freshChrome: 'site',
+  },
+  urls: [
+    'https://emergency.johanedlund.se',
+    'https://emergency.johanedlund.se/posts/2020-03-19-about-this/',
+    'https://emergency.johanedlund.se/posts/2020-03-20-getting-started/',
+  ],
+};
--- _data/sites/johanedlund.js
+++ _data/sites/johanedlund.js
@@ -2,8 +2,11 @@ module.exports = {
   name: 'johanedlund.se',
   description: 'johanedlund.se',
   options: {
-    frequency: 60 * 2, // 2 hour
+    frequency: 60 * 23, // 24 hour
     freshChrome: 'site',
   },
-  urls: ['https://www.johanedlund.se/', 'https://www.johanedlund.se/posts/this-is-the-first-example-post/'],
+  urls: [
+    'https://www.johanedlund.se/',
+    'https://www.johanedlund.se/posts/this-is-the-first-example-post/',
+  ],
 };
