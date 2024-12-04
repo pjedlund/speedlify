a4ce9f4c856c97b6795da7430014f31b105f242d
Johan Edlund <me@johanedlund.se>
2024-12-04 22:07:28 +0100

    Run build webhook at midnight every day

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -2,13 +2,14 @@ import fetch from 'node-fetch';
 import { schedule } from '@netlify/functions';
 
 // This is a sample build hook URL
-const BUILD_HOOK =
-  'https://api.netlify.com/build_hooks/674f6b8766f21446c6af0f79';
+//const BUILD_HOOK = 'https://api.netlify.com/build_hooks/674f6b8766f21446c6af0f79';
+const BUILD_HOOK = process.env.NETLIFY_REBUILD_HOOK;
 
 // Schedules the handler function to run at midnight on
 // Mondays, Wednesday, and Friday 0 0 * * 1,5
 // Every 10 minutes */10 * * * *
-const handler = schedule('*/10 * * * *', async () => {
+// Every day at midnight 0 0 * * *
+const handler = schedule('0 0 * * *', async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST',
   }).then((response) => {
